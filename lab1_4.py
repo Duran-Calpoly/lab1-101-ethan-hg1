369f9e5278474ad3886577b8307bff39aa87a58f
Ethan G <ethanhg.pnw@gmail.com>
2026-01-15 01:13:00 +0000

    lab1_4 with input operations

--- lab1_4.py
+++ lab1_4.py
@@ -1,6 +1,12 @@
-calculate_average = (num1 + num2 + num3)/3;
+def calculate_average():
+    num1 = input(int = "pick num1: ");
+    num2 = input(int = "pick num2: ");
+    num3 = input(int = "pick num3: ");
+    return (num1 + num2 + num3)/3;
 
-add_tax = bill_total * 1.1;
+def add_tax():
+    bill_total = input(int = "meal cost: ");
+    return bill_total * 1.1;
 
 def greet_user():
     name = input("name: ");
