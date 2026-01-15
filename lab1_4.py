c29db9d5848eb21c6d32f728148b1031626527a6
Ethan G <ethanhg.pnw@gmail.com>
2026-01-15 01:31:40 +0000

    lab1_4 assert and annotations

--- lab1_4.py
+++ lab1_4.py
@@ -1,13 +1,15 @@
-def calculate_average():
-    num1 = input(int = "pick num1: ");
-    num2 = input(int = "pick num2: ");
-    num3 = input(int = "pick num3: ");
-    return (num1 + num2 + num3)/3;
+def calculate_average(num1, num2, num3):
+    return (num1 + num2 + num3)/3 
+#calculates avg of 3 numbers
 
-def add_tax():
-    bill_total = input(int = "meal cost: ");
-    return bill_total * 1.1;
+def add_tax(bill_total):
+    return bill_total * 1.1
+#calculates a 10% tip
 
-def greet_user():
-    name = input("name: ");
-    return "Hello " + name;
+def greet_user(name):
+    return "Hello " + name
+#greets user
+
+assert(calculate_average(3, 4, 5), 4)
+assert(add_tax(10), 11)
+assert(greet_user("Ethan"), "Hello Ethan")
